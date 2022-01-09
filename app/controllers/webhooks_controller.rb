0a5c6133c9450067dbea1e598f27525e213654e7
harryuan65 <harryuan.65@gmail.com>
2022-01-10 00:19:25 +0800

    feat: add webhook route, action, test creds

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,36 @@
+class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
+    endpoint_secret = Rails.application.credentials.stripe[:webhook_key]
+
+    begin
+      event = Stripe::Webhook.construct_event(
+        payload, sig_header, endpoint_secret
+      )
+    rescue JSON::ParserError => e
+      # Invalid payload
+      return render json: { message: e }, status: :bad_request
+    rescue Stripe::SignatureVerificationError => e
+      # Invalid signature
+      return render json: { message: e }, status: :bad_request
+    end
+
+    # Handle the event
+    case event.type
+    when 'payment_intent.succeeded'
+      puts 'PaymentIntent was succeeded'
+    when 'payment_method.attached'
+      puts 'PaymentMethod was attached to a Customer!'
+    else
+      puts "Unhandled event type: #{event.type}"
+    end
+    # stuff here
+    File.open(Rails.root.join("spec/fixtures/#{event.type}.yml"), 'a') do |f|
+      f.write event.data.object.as_json.to_yaml
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   root 'static_pages#root'
+  resources :webhooks, only: %i[create]
+  # post '/webhooks', to: 'webhooks#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
